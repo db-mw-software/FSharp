a72bd3e79752fd37c8aae64a05c6e219d12526e7
Daniel <danve4@gmail.com>
2019-04-07 16:32:39 -0500

    Adding routes for users and controllers to handle them

--- webserver/routes/user.js
+++ webserver/routes/user.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user');
 
-router.get('/genres', (req, res) => {
-  res.send('Hello from user genres!');
-});
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+
+router.get('/genres', userController.genres);
+router.post('/genres', userController.saveGenres);
+
+router.get('/artists', userController.artists);
+router.post('/artists', userController.saveArtists);
+
+router.get('/information', userController.information);
+router.put('/information', userController.saveInformation);
+
+router.get('/recommendations', userController.recommendations);
+router.post('/concert-interest', userController.concertInterest);
+
+router.post('/search', userController.search);
+router.post('/follow',userController.follow);
 
 module.exports = router;
