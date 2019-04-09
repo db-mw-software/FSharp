1ecc38c075b7a284f390656c9ef32dd23bdcef6c
Daniel <danve4@gmail.com>
2019-04-09 02:15:13 +0000

    Adding templates for user model related functions

--- webserver/controllers/user.js
+++ webserver/controllers/user.js
@@ -1,51 +1,66 @@
-exports.register = (req, res, next) => {
-  res.send('Hello from user register!');
+const userModel = require('../model/user');
+
+exports.register = async (req, res, next) => {
+  const registerModel = await userModel.register('');
+  res.send('Hello from user register! And from ' + registerModel);
 }
 
-exports.login = (req, res, next) => {
-  res.send('Hello from user login!');
+exports.login = async (req, res, next) => {
+  const loginModel = await userModel.login('');
+  res.send('Hello from user login! And from ' + loginModel);
 }
 
-exports.logout = (req, res, next) => {
-  res.send('Hello from user logout!');
+exports.logout = async (req, res, next) => {
+  const logoutModel = await userModel.logout();
+  res.send('Hello from user logout! And from ' + logoutModel);
 }
 
-exports.genres = (req, res, next) => {
-  res.send('Hello from user genres!');
+exports.genres =  async (req, res, next) => {
+  const genreModel = await userModel.genres();
+  res.send('Hello from user genres! And from ' + genreModel);
 }
 
-exports.saveGenres = (req, res, next) => {
-  res.send('Hello from user save genres!');
+exports.saveGenres = async (req, res, next) => {
+  const saveGenresModel = await userModel.saveGenres();
+  res.send('Hello from user save genres! And from ' + saveGenresModel);
 }
 
-exports.artists = (req, res, next) => {
-  res.send('Hello from user artists!');
+exports.artists = async (req, res, next) => {
+  const artistsModel = await userModel.artists();
+  res.send('Hello from user artists! And from ' + artistsModel);
 }
 
-exports.saveArtists = (req, res, next) => {
-  res.send('Hello from user save artists!');
+exports.saveArtists = async (req, res, next) => {
+  const saveArtistsModel = await userModel.saveArtists();
+  res.send('Hello from user save artists! And from ' + saveArtistsModel);
 }
 
-exports.information = (req, res, next) => {
-  res.send('Hello from user information!');
+exports.information = async (req, res, next) => {
+  const informationModel = await userModel.information();
+  res.send('Hello from user information! And from ' + informationModel);
 }
 
-exports.saveInformation = (req, res, next) => {
-  res.send('Hello from user save information!');
+exports.saveInformation = async (req, res, next) => {
+  const saveInformationModel = await userModel.saveInformation();
+  res.send('Hello from user save information! And from ' + saveInformationModel);
 }
 
-exports.recommendations = (req, res, next) => {
-  res.send('Hello from user recommendations!');
+exports.recommendations = async (req, res, next) => {
+  const recommendationsModel = await userModel.recommendations();
+  res.send('Hello from user recommendations! And from ' + recommendationsModel);
 }
 
-exports.concertInterest = (req, res, next) => {
-  res.send('Hello from user concert interest!');
+exports.concertInterest = async (req, res, next) => {
+  const concertInterestModel = await userModel.concertInterest();
+  res.send('Hello from user concert interest! And from ' + concertInterestModel);
 }
 
-exports.search = (req, res, next) => {
-  res.send('Hello from user search!');
+exports.search = async (req, res, next) => {
+  const searchModel = await userModel.search();
+  res.send('Hello from user search! And from ' + searchModel);
 }
 
-exports.follow = (req, res, next) => {
-  res.send('Hello from user follow!');
+exports.follow = async (req, res, next) => {
+  const followModel = await userModel.follow();
+  res.send('Hello from user follow! And from ' + followModel);
 }
