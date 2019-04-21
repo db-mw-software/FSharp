e698c5e2666a79a603a625c012b770b94b68c483
Daniel Vega <danve4@gmail.com>
2019-04-20 21:22:26 -0500

    Adding example for controller returning database query result

--- webserver/controllers/music.js
+++ webserver/controllers/music.js
@@ -1,9 +1,12 @@
+const musicModel = require('../model/music');
+
 exports.genres = (req, res, next) => {
   res.send('Hello from music genres!');
 }
 
-exports.artists = (req, res, next) => {
-  res.send('Hello from music artists!');
+exports.artists = async (req, res, next) => {
+  const artistArray = await musicModel.artists();
+  res.send(artistArray);
 }
 
 exports.concerts = (req, res, next) => {
