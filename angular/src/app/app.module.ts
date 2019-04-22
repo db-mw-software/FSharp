b101fec449d421c52d6ec02fb186627243e49cd9
Daniel <danve4@gmail.com>
2019-04-21 23:05:05 -0500

    Adding Angular Material

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArtisttrainComponent } from './artisttrain/artisttrain.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ArtisttrainComponent } from './artisttrain/artisttrain.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
