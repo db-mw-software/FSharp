849709dd66b56f18404af5e1791f4d0f7cdb937a
Daniel Vega <danve4@gmail.com>
2019-04-24 00:38:10 -0500

    Adding routing for new components

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { GenretrainComponent } from './genretrain/genretrain.component';
 import { ArtisttrainComponent } from './artisttrain/artisttrain.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'genretrain', component: GenretrainComponent },
   { path: 'artisttrain', component: ArtisttrainComponent }
 ];
 
